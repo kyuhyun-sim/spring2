7b8e2380b8bbf4414cbe45ac19928b0021042cc6
kyuhyun-sim <zaq7974@naver.com>
2022-08-03 10:13:32 +0900

    유저 서비스를 유저 컨트롤러에 적용

--- spring2/src/main/java/com/sparta/spring2/controller/UserController.java
+++ spring2/src/main/java/com/sparta/spring2/controller/UserController.java
@@ -1,11 +1,19 @@
 package com.sparta.spring2.controller;
 
 import com.sparta.spring2.dto.SignupRequestDto;
+import com.sparta.spring2.servic.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class UserController {
+    private final UserService userService;
+
+    @Autowired
+    public UserController(UserService userService) {
+        this.userService = userService;
+    }
     //회원가입 post API
     @PostMapping("/api/member/signup")
     public String registerUser(SignupRequestDto requestDto) {
